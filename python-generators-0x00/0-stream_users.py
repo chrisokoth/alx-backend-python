753008a010d06bca3076ee1a8051de98fad317ad
chrisokoth <chrisokoth17@gmail.com>
2024-12-06 01:51:08 +0300

    sql database generator

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,24 @@
+
+import mysql.connector
+
+def connect_db():
+    """Connect to the MySQL database."""
+    return mysql.connector.connect(
+        host="localhost",  
+        user="root",       
+        password="okothee07",  
+        database="ALX_prodev"
+    )
+
+def stream_users():
+    """Generator function to stream users one by one."""
+    connection = connect_db()
+    cursor = connection.cursor(dictionary=True)  
+    cursor.execute("SELECT user_id, name, email, age FROM user_data")
+
+    # Yield each row one by one
+    for row in cursor:
+        yield row
+
+    cursor.close()
+    connection.close()
